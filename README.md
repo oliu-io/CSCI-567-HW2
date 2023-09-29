e373e7f76fcbf87570bfa7e1045ece29ca9587f2
Oliver Liu <oliver.liu.misc@gmail.com>
2023-09-29 00:39:36 +0000

    BYOB initial push

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Bring Your Own BERT
 
-In this assignment, we will implement our own BERT model from scratch.
+In this assignment, we will implement our own BERT model from scratch! This repo is adapted from materials provided by Redwood Research[https://www.redwoodresearch.org/mlab].
+
+To get started, first clone this repo and run
+```
+pip install -r requirements.txt
+```
+
+You can find all the TODO's in `bert_impl.py`. Search for `NotImplementedError` and work through each of the code segment. To verify your implementation, run
+```
+python bert_impl.py
+```
+for executing test functions. We recommend you do this after each segment!
---
+++ utils.py
@@ -0,0 +1,32 @@
+from einops.einops import rearrange
+import torch as t
+
+def itpeek(tensor: t.Tensor):
+    contains_nan = t.any(t.isnan(tensor)).item()
+    contains_inf = t.any(t.isinf(tensor)).item()
+    string = f"SHAPE {tuple(tensor.shape)} MEAN: {'{0:.4g}'.format(t.mean(tensor.float()).cpu().item())} STD: {'{0:.4g}'.format(t.std(tensor.float()).cpu().item())} {'CONTAINS_NAN! ' if contains_nan else ''}{'CONTAINS_INF! ' if contains_inf else ''}VALS [{' '.join(['{0:.4g}'.format(x) for x in t.flatten(tensor)[:10].cpu().tolist()])}{'...' if tensor.numel()>10 else ''}]"
+    return string
+
+def tpeek(name: str, tensor: t.Tensor, ret: bool = False):
+    string = f"{name} {itpeek(tensor)}"
+    if ret:
+        return string
+    print(string)
+
+def has_not_null(obj, prop):
+    return hasattr(obj, prop) and (getattr(obj, prop) is not None)
+
+
+def copy_weight_bias(mine, theirs, transpose=False):
+    if transpose:
+        mine.weight = t.nn.Parameter(rearrange(theirs.weight, "a b -> b a"))
+    else:
+        mine.weight = theirs.weight
+
+    theirs_has_bias = has_not_null(theirs, "bias")
+    mine_has_bias = has_not_null(mine, "bias")
+    if theirs_has_bias != mine_has_bias:
+        print(mine.bias)
+        raise AssertionError("yikes")
+    if mine_has_bias and theirs_has_bias:
+        mine.bias = theirs.bias
